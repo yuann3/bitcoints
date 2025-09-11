382a0b93ed027103de32c7d4573ef61bae5d32e7
Yiyuan Li <yy@eyuan.me>
2025-09-11 21:59:52 +0800

    tokio

--- lib/src/network.rs
+++ lib/src/network.rs
@@ -54,4 +54,21 @@ impl Message {
     pub fn decode(data: &[u8]) -> Result<Self, ciborium::de::Error<IoError>> {
         ciborium::from_reader(data)
     }
+
+    pub fn send(&self, stream: &mut impl Write) -> Result<(), ciborium::ser::Error<IoError>> {
+        let bytes = self.encode()?;
+        let len = bytes.len() as u64;
+        stream.write_all(&len.to_be_bytes())?;
+        stream.write_all(&bytes)?;
+        Ok(())
+    }
+
+    pub fn receive(stream: &mut impl Read) -> Result<Self, ciborium::de::Error<IoError>> {
+        let mut len_bytes = [0u8; 8];
+        stream.read_exact(&mut len_bytes)?;
+        let len = u64::from_be_bytes(len_bytes) as usize;
+        let mut data = vec![0u8; len];
+        stream.read_exact(&mut data)?;
+        Self::decode(&data)
+    }
 }
--- miner/src/main.rs
+++ miner/src/main.rs
@@ -1,14 +1,31 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use btclib::crypto::PublicKey;
+use btclib::network::Message;
+use btclib::util::Saveable;
+use std::env;
+use std::process::exit;
+use tokio::net::TcpStream;
+
+fn usage() -> ! {
+    eprintln!("Usage: {} <address> <public_key_file>", env::args().next().unwrap());
+    exit(1);
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[tokio::main]
+async fn main() {
+    let address = match env::args().nth(1) {
+        Some(address) => address,
+        None => usage(),
+    };
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    let public_key_file = match env::args().nth(2) {
+        Some(public_key_file) => public_key_file,
+        None => usage(),
+    };
+
+    let Ok(public_key) = PublicKey::load_from_file(&public_key_file) else {
+        eprint!("Error reading public key from file {}", public_key_file);
+        exit(1);
+    };
+
+    println!("Connecting to {address} to mine with {public_key:?}");
 }
